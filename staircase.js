c9020ac522f8e8d1ce9ee48c2fc5c26227d8a105
Mateo Ryhr <mateoryhr29@gmail.com>
2021-02-20 13:33:07 -0300

    Staircase solved.

--- staircase.js
+++ staircase.js
@@ -1,1 +1,18 @@
 // Problem: https://www.hackerrank.com/challenges/staircase/problem
+
+function printStaircase(n){
+    //print a line
+    for(let i=1;i<=n;i++){
+        //print spaces
+        for(let j=n-i;j>0;j--){
+            process.stdout.write(' ')
+        }
+        //print #
+        for(let k=1;k<=i;k++){
+            process.stdout.write('#')
+        }
+        process.stdout.write('\n')
+    }
+}
+
+printStaircase(20)
